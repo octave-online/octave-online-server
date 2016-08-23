ffdd33f402d6167fd06ed1a3d3aa295df05e1f92
Octave Online Webmaster <webmaster@octave-online.net>
2016-08-23 06:24:36 +0000

    Improving email token sign-in flow UI

--- client/app/js/runtime.js
+++ client/app/js/runtime.js
@@ -5,22 +5,28 @@ require.config({
 	paths: {
 		// Social Plugins
 		"twitter": [
+			"https://platform.twitter.com/widgets",
 			"http://platform.twitter.com/widgets",
 			"/js-default/twitter"
 		],
 		"addthis": [
+			"https://s7.addthis.com/js/300/addthis_widget", // #pubid=ra-51d65a00598f1528
 			"http://s7.addthis.com/js/300/addthis_widget", // #pubid=ra-51d65a00598f1528
 			"/js-default/addthis"
 		],
 		"uservoice": [
+			"https://widget.uservoice.com/5fPlQ3CsHcjp8LVE8dAeA",
 			"http://widget.uservoice.com/5fPlQ3CsHcjp8LVE8dAeA",
 			"/js-default/uservoice"
 		],
 		"analytics": [
+			"https://www.google-analytics.com/analytics",
 			"http://www.google-analytics.com/analytics",
+			"https://cdn.octave-online.com/analytics-dummy",
 			"http://cdn.octave-online.com/analytics-dummy"
 		],
 		"webfont": [
+			"https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont",
 			"http://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont",
 			"/js-default/webfont"
 		],
