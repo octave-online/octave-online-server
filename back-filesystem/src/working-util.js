5776deb6e225d7a4bdfc7040e0e5cc8a4b311f0f
Shane F. Carr <shane.carr@wustl.edu>
2016-12-17 00:02:09 -0800

    Adding recursive file listing to give basic directory support.

--- back-filesystem/src/working-util.js
+++ back-filesystem/src/working-util.js
@@ -8,6 +8,7 @@ const charsetDetector = require("node-icu-charset-detector");
 const Iconv = require("iconv").Iconv;
 const logger = require("@oo/shared").logger;
 const config = require("@oo/shared").config;
+const mkdirp = require("mkdirp");
 
 // Load extra MIME types
 mime.load(path.join(__dirname, "mime.types"));
@@ -25,10 +26,7 @@ class WorkingUtil {
 	listAll(next) {
 		async.waterfall([
 			(_next) => {
-				fs.readdir(this.cwd, _next);
-			},
-			(files, _next) => {
-				async.map(files, this.getFileInfo.bind(this), _next);
+				this._recursiveReaddir(this.cwd, 0, _next);
 			},
 			(fileInfos, _next) => {
 				const dict = {};
@@ -43,49 +41,77 @@ class WorkingUtil {
 		], next);
 	}
 
-	getFileInfo(filename, next) {
-		const _mime = mime.lookup(filename);
-		if (ACCEPTABLE_MIME_REGEX.test(_mime)) {
-			async.waterfall([
-				(_next) => {
-					fs.stat(path.join(this.cwd, filename), _next);
-				},
-				(stats, _next) => {
-					if (stats.size > config.session.textFileSizeLimit) {
-						// This file is too big.  Do not perform any further processing on this file.
-						// FIXME: Show a nice message to the end user to let them know why their file isn't being loaded
-						this._log.debug("Skipping text file that is too big:", stats.size, filename);
-						return next(null, {
-							filename,
-							isText: false
-						});
-					}
+	_recursiveReaddir(directory, depth, next) {
+		// Don't recurse more than 3 levels deep
+		if (depth > 3) {
+			return next(null, []);
+		}
 
-					// The file is small.  Continue processing.
-					fs.readFile(path.join(this.cwd, filename), _next);
-				},
-				(buf, _next) => {
-					this._convertCharset(buf, _next)
-				},
-				(buf, _next) => {
-					_next(null, {
-						filename,
+		async.waterfall([
+			(_next) => {
+				fs.readdir(directory, _next);
+			},
+			(files, _next) => {
+				async.map(files, (filename, __next) => {
+					let pathname = path.join(directory, filename);
+					let relname = path.relative(this.cwd, pathname);
+					if (pathname === path.join(this.cwd, ".git")) return;
+					async.waterfall([
+						(___next) => {
+							// lstat to prevent following symlinks
+							fs.lstat(pathname, ___next);
+						},
+						(stats, ___next) => {
+							if (stats.isDirectory()) {
+								return this._recursiveReaddir(pathname, depth+1, ___next);
+							} else if (stats.isFile()) {
+								return this._getFileInfo(filename, pathname, relname, stats, ___next);
+							} else {
+								return ___next(null, []);
+							}
+						}
+					], __next);
+				}, (err, results) => {
+					if (err) return _next(err);
+					_next(null, Array.prototype.concat.apply([], results));
+				});
+			}
+		], next);
+	}
+
+	_getFileInfo(filename, pathname, relname, stats, next) {
+		let _mime = mime.lookup(filename);
+		if (ACCEPTABLE_MIME_REGEX.test(_mime)) {
+			if (stats.size > config.session.textFileSizeLimit) {
+				// This file is too big.  Do not perform any further processing on this file.
+				// FIXME: Show a nice message to the end user to let them know why their file isn't being loaded
+				this._log.debug("Skipping text file that is too big:", stats.size, filename);
+				next(null, [{
+					filename: relname,
+					isText: false
+				}]);
+			} else {
+				fs.readFile(pathname, (err, buf) => {
+					if (err) return ___next(err);
+					buf = this._convertCharset(buf);
+					next(null, [{
+						filename: relname,
 						isText: true,
 						content: buf.toString("base64")
-					});
-				}
-			], next);
+					}]);
+				});
+			}
 		} else if (!UNACCEPTABLE_FILENAME_REGEX.test(filename)) {
-			return next(null, {
-				filename,
+			next(null, [{
+				filename: relname,
 				isText: false
-			});
+			}]);
 		} else {
-			return next(null, null);
+			next(null, []);
 		}
 	}
 
-	_convertCharset(buf, next) {
+	_convertCharset(buf) {
 		var encoding;
 
 		// Detect and attempt to convert charset
@@ -104,27 +130,32 @@ class WorkingUtil {
 		// TODO: Is there a better way than converting to a string here?
 		buf = new Buffer(buf.toString("utf8").replace(/\r\n/g, "\n"));
 
-		return next(null, buf);
+		return buf;
 	}
 
 	saveFile(filename, value, next) {
 		// Create backup of file in memory in case there are any I/O errors
 		async.waterfall([
 			(_next) => {
+				let dirname = path.dirname(this._safePath(filename));
+				mkdirp(dirname, _next);
+			},
+			(_, _next) => {
 				fs.readFile(
-					path.join(this.cwd, filename),
-						(err, buf) => {
+					this._safePath(filename),
+					(err, buf) => {
 						if (!err) return _next(null, buf);
 						if (/ENOENT/.test(err.message)) {
 							this._log.info("Creating new file:", filename);
 							return _next(null, new Buffer(0));
 						}
 						return _next(err);
-					});
+					}
+				);
 			},
 			(buf, _next) => {
 				fs.writeFile(
-					path.join(this.cwd, filename),
+					this._safePath(filename),
 					value,
 					(err) => {
 						_next(null, buf, err);
@@ -133,7 +164,7 @@ class WorkingUtil {
 			(buf, err, _next) => {
 				if (err) {
 					fs.writeFile(
-						path.join(this.cwd, filename),
+						this._safePath(filename),
 						buf,
 						() => {
 							_next(err);
@@ -149,21 +180,21 @@ class WorkingUtil {
 
 	renameFile(oldname, newname, next) {
 		fs.rename(
-			path.join(this.cwd, oldname),
-			path.join(this.cwd, newname),
+			this._safePath(oldname),
+			this._safePath(newname),
 			next);
 	}
 
 	deleteFile(filename, next) {
 		fs.unlink(
-			path.join(this.cwd, filename),
+			this._safePath(filename),
 			next);
 	}
 
 	readBinary(filename, next) {
 		async.waterfall([
 			(_next) => {
-				fs.readFile(path.join(this.cwd, filename), _next);
+				fs.readFile(this._safePath(filename), _next);
 			},
 			(buf, _next) => {
 				const base64data = buf.toString("base64");
@@ -171,6 +202,16 @@ class WorkingUtil {
 				_next(null, base64data, _mime);
 			}
 		], next);
+	}
+
+	_safePath(filename) {
+		// Filenames must descend from the working directory.  Forbids filenames starting with '..' or similar.
+		let candidate = path.join(this.cwd, filename);
+		if (candidate.substring(0, this.cwd.length) !== this.cwd) {
+			this._log.warn("Processed a fishy filename:", filename);
+			return path.join(this.cwd, "octave-workspace"); // an arbitrary legal path
+		}
+		return candidate;
 	}
 }
 
