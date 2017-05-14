564bbd410296b932349a368bf27a132dbf5dfc65
Octave Online Utils <webmaster@octave-online.net>
2017-05-14 03:49:24 +0000

    Adding benchmarks and static file support to utils-auth

--- utils-auth/app.js
+++ utils-auth/app.js
@@ -12,6 +12,7 @@ const fs = require("fs");
 //const PORT = 5123;
 const SOCKET_PATH = "/var/run/oosocks/auth.sock";
 try { fs.unlinkSync(SOCKET_PATH); } catch(err) {}
+console.log("Listening on UNIX socket " + SOCKET_PATH);
 
 mongoose.connect("mongodb://127.0.0.1: " + config.mongo.port + "/" + config.mongo.db);
 const User = mongoose.model("User", {
@@ -20,15 +21,24 @@ const User = mongoose.model("User", {
 	password_hash: String
 });
 
-console.log("Listening on UNIX socket " + SOCKET_PATH);
+var benchmarkMongoAvg = 0.0;
+var benchmarkMongoCnt = 0;
+var benchmarkBcryptAvg = 0.0;
+var benchmarkBcryptCnt = 0;
+
 const server = http.createServer((req, res) => {
 	const originalUri = path.normalize(req.headers["x-original-uri"] || "");
-	const match = /^\/(\w+)\.git\/.+$/.exec(originalUri);
-	if (match === null) {
+	const match1 = /^\/(\w+)\.git\/.*$/.exec(originalUri);
+	const match2 = /^\/(src|themes|vendor)\/.*$/.exec(originalUri);
+	if (match2 !== null) {
+		res.writeHead(204);
+		return res.end();
+	}
+	if (match1 === null) {
 		res.writeHead(403);
 		return res.end();
 	}
-	const parametrized = match[1];
+	const parametrized = match1[1];
 	if (req.headers["authorization"]) {
 		// Check username/password
 		const creds = basicAuth.parse(req.headers["authorization"]);
@@ -36,7 +46,11 @@ const server = http.createServer((req, res) => {
 			res.writeHead(400);
 			return res.end("Invalid credentials");
 		}
+		const mongoStart = new Date().valueOf();
 		User.findOne({ email: creds.name }, (err, user) => {
+			const mongoEnd = new Date().valueOf();
+			benchmarkMongoAvg = benchmarkMongoAvg + (mongoEnd-mongoStart-benchmarkMongoAvg)/(benchmarkMongoCnt+1);
+			benchmarkMongoCnt++;
 			if (err || !user) {
 				console.error(err || "User not found");
 				return prompt(res, "Make sure you entered the correct email/password");
@@ -47,7 +61,11 @@ const server = http.createServer((req, res) => {
 			if (user.parametrized !== parametrized) {
 				return prompt(res, "Make sure your repository path is correct");
 			}
+			const bcryptStart = new Date().valueOf();
 			bcrypt.compare(creds.pass, user.password_hash, (err, valid) => {
+				const bcryptEnd = new Date().valueOf();
+				benchmarkBcryptAvg = benchmarkBcryptAvg + (bcryptEnd-bcryptStart-benchmarkBcryptAvg)/(benchmarkBcryptCnt+1);
+				benchmarkBcryptCnt++;
 				if (err) {
 					console.error(err);
 					res.writeHead(400);
@@ -73,3 +91,8 @@ function prompt(res, message) {
 	});
 	return res.end();
 }
+
+setInterval(() => {
+	console.log("Mongo: " + benchmarkMongoAvg + " (" + benchmarkMongoCnt + " reqs); "
+	           +"Bcrypt: "+ benchmarkBcryptAvg+ " (" + benchmarkBcryptCnt+ " reqs)");
+}, 86400000);
