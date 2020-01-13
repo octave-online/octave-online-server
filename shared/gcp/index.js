e94fc5ab68c447e6a5cb6e404224d607fd50baf4
Shane F. Carr <shane.carr@wustl.edu>
2020-01-13 10:35:43 +0100

    Initial code for automated instance removal from GCP groups

---
+++ shared/gcp/index.js
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2019, Octave Online LLC
+ *
+ * This file is part of Octave Online Server.
+ *
+ * Octave Online Server is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Octave Online Server is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Octave Online Server.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+const Compute = require("@google-cloud/compute");
+const gcpMetadata = require("gcp-metadata");
+
+const config = require("@oo/shared").config;
+
+let _computeClient = null;
+
+function getComputeClient() {
+	if (!_computeClient) {
+		if (config.gcp.key_filename) {
+			const keyInfo = require(config.gcp.key_filename);
+			_computeClient = new Compute({
+				projectId: keyInfo.project_id,
+				keyFilename: config.gcp.key_filename
+			});
+		} else {
+			_computeClient = new Compute();
+		}
+	}
+	return _computeClient;
+}
+
+async function getRecommendedSize() {
+	const client = getComputeClient()
+		.zone(config.gcp.zone)
+		.autoscaler(config.gcp.instance_group_name);
+	const [ _, result ] = await client.get();
+	console.log("Recommended size:", result.recommendedSize);
+	return result.recommendedSize;
+}
+
+async function getTargetSize() {
+	const client = getComputeClient()
+		.zone(config.gcp.zone)
+		.instanceGroupManager(config.gcp.instance_group_name);
+	const [ _, result ] = await client.get();
+	console.log("Target size:", result.targetSize);
+	return result.targetSize;
+}
+
+async function getAutoscalerInfo() {
+	const [ recommendedSize, targetSize ] = await Promise.all([
+		getRecommendedSize(),
+		getTargetSize()
+	]);
+	return {
+		recommendedSize,
+		targetSize
+	};
+}
+
+async function getSelfName() {
+	const name = await gcpMetadata.instance("name");
+	console.log("Self name:", name);
+	return name;
+}
+
+async function removeSelfFromGroup() {
+	const selfName = await getSelfName();
+	const vm = getComputeClient()
+		.zone(config.gcp.zone)
+		.vm(selfName);
+	const client = getComputeClient()
+		.zone(config.gcp.zone)
+		.instanceGroupManager(config.gcp.instance_group_name);
+
+	let operation;
+	if (config.gcp.instance_group_removal_method === "abandon") {
+		console.log("Abandoning self");
+		[ operation ] = await client.abandonInstances(vm);
+	} else if (config.gcp.instance_group_removal_method === "delete") {
+		console.log("Deleting self");
+		[ operation ] = await client.deleteInstances(vm);
+	} else {
+		throw new Error("Unknown removal method");
+	}
+	return operation;
+}
+
+module.exports = {
+	getAutoscalerInfo,
+	removeSelfFromGroup
+};
