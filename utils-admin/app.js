1d6748efd88f15f819bfe7bbeaa923a9740303ff
Shane F. Carr <shane.carr@wustl.edu>
2020-02-25 00:07:58 -0800

    Add HTTP basic auth directly to utils-admin Express server

--- utils-admin/app.js
+++ utils-admin/app.js
@@ -18,22 +18,25 @@
  * <https://www.gnu.org/licenses/>.
  */
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const basicAuth = require("express-basic-auth");
+const config = require("@oo/shared").config;
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
+app.use(basicAuth({ users: config.auth.utils_admin.users, challenge: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
