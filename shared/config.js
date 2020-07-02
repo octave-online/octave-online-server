df873b9dcdc6d52c3571f1d74e799408b0e7433f
Shane F. Carr <shane.carr@wustl.edu>
2020-07-02 20:11:35 +0000

    Checkpoint for oos-quick-start

--- shared/config.js
+++ shared/config.js
@@ -25,17 +25,19 @@ const path = require("path");
 const hjson = require("hjson");
 const deepmerge = require("deepmerge");
 
+const log = require("./logger")("config");
+
 const defaultConfig = hjson.parse(fs.readFileSync(path.join(__dirname, "..", "config_defaults.hjson")).toString("utf-8"));
 
 try {
 	var configBuffer = fs.readFileSync(path.join(__dirname, "..", "config.hjson"));
 } catch(e) {
-	// The process will die here if config.hjson is not found, so console is OK
+	const message = "Could not read config.hjson. Octave Online Server will use all default settings.";
+	log.warn(message);
+	// Allow console to make sure the message always gets printed, even when not in debug mode:
 	// eslint-disable-next-line no-console
-	console.error("Could not read config.hjson. Please create it in the project directory with desired setting overrides.");
-	// eslint-disable-next-line no-console
-	console.error(e);
-	process.exit(1);
+	console.error("Notice: " + message);
+	configBuffer = Buffer.alloc(0);
 }
 
 try {
