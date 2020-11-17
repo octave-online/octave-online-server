a8bb8236e623aec8a1496181c41d11ad461b1941
Shane F. Carr <shane.carr@wustl.edu>
2020-11-17 01:53:21 -0600

    create-repo-service: Show different messages if directory exists

--- back-filesystem/git/create-repo-service.js
+++ back-filesystem/git/create-repo-service.js
@@ -25,6 +25,7 @@
 /* eslint-disable no-console */
 
 const child_process = require("child_process");
+const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
@@ -51,28 +52,50 @@ http.createServer((req, res) => {
 		return res.end();
 	}
 	const bareRepoPath = path.join(gitRoot, query.type, query.name + ".git");
-	const process = (query.action === "delete")
-		? child_process.spawn("rm", ["-rf", bareRepoPath])
-		: child_process.spawn("git", ["init", "--bare", bareRepoPath]);
-	let resData = Buffer.alloc(0);
-	process.stdout.on("data", (chunk) => {
-		resData = Buffer.concat([resData, chunk]);
-	});
-	process.stderr.on("data", (chunk) => {
-		resData = Buffer.concat([resData, chunk]);
-	});
-	process.on("exit", (code /* , signal */) => {
-		const operation = (query.action === "delete") ? "Delete" : "Init";
-		if (code === 0) {
-			res.writeHead(200, { "Content-Type": "text/plain" });
-			console.log(`create-repo-service: ${isoTime} ${operation} Success: ${bareRepoPath}`);
+	fs.stat(bareRepoPath, (err) => {
+		const exists = !err;
+		let process;
+		if (query.action === "delete") {
+			if (exists) {
+				process = child_process.spawn("rm", ["-rf", bareRepoPath]);
+			} else {
+				res.writeHead(200, { "Content-Type": "text/plain" });
+				res.end("Already Deleted\n");
+				console.log(`create-repo-service: ${isoTime} Already Deleted: ${bareRepoPath}`);
+				return;
+			}
 		} else {
-			res.writeHead(500, { "Content-Type": "text/plain" });
-			console.log(`create-repo-service: ${isoTime} ${operation} Error: ${bareRepoPath}`);
-			console.log(`create-repo-service: ${resData.toString("utf-8")}`);
+			if (exists) {
+				res.writeHead(200, { "Content-Type": "text/plain" });
+				res.end("Already Created\n");
+				console.log(`create-repo-service: ${isoTime} Already Created: ${bareRepoPath}`);
+				return;
+			} else {
+				process = child_process.spawn("git", ["init", "--bare", bareRepoPath]);
+			}
 		}
-		res.write(resData);
-		res.end();
+		let resData = Buffer.alloc(0);
+		process.stdout.on("data", (chunk) => {
+			resData = Buffer.concat([resData, chunk]);
+		});
+		process.stderr.on("data", (chunk) => {
+			resData = Buffer.concat([resData, chunk]);
+		});
+		process.on("exit", (code /* , signal */) => {
+			const operation = (query.action === "delete") ? "Delete" : "Init";
+			if (code === 0) {
+				res.writeHead(200, { "Content-Type": "text/plain" });
+				res.write(`${operation} Success\n`);
+				console.log(`create-repo-service: ${isoTime} ${operation} Success: ${bareRepoPath}`);
+			} else {
+				res.writeHead(500, { "Content-Type": "text/plain" });
+				res.write(`${operation} Error\n`);
+				console.log(`create-repo-service: ${isoTime} ${operation} Error: ${bareRepoPath}`);
+				console.log(`create-repo-service: ${resData.toString("utf-8")}`);
+			}
+			res.write(resData);
+			res.end();
+		});
 	});
 }).listen(port);
 
